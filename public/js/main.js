8515a5f493f500434219a72b42217294cc7eff3b
Kouadio Bah Rodolphe <rodolphekouadio225@icloud.com>
2025-08-01 10:49:51 +0000

    correction front

--- public/js/main.js
+++
@@ -1,9 +0,0 @@
-import './theme.js';
-import './ai.js';
-import './sidebar.js';
-import { initNotes } from './notes.js';
-import './folders.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initNotes(); // Initialise la gestion des notes
-});
--- public/js/storage.js
+++
@@ -1,7 +0,0 @@
-export function saveNotes(notes) {
-    localStorage.setItem('notes', JSON.stringify(notes));
-}
-
-export function loadNotes() {
-    return JSON.parse(localStorage.getItem('notes')) || [];
-}
